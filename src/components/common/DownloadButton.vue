62efa80327fc72c67c866889784edd4ee2ab7e31
Yinjie <40953292+yinjie-z@users.noreply.github.com>
2026-03-02 22:16:41 +0100

    merged

--- src/components/common/DownloadButton.vue
+++ src/components/common/DownloadButton.vue
@@ -8,7 +8,9 @@ const { currentLang } = useLanguage()
   <button
     class="bg-blue-700 hover:bg-blue-600 text-white px-8 py-3 rounded-md font-semibold transition-all"
   >
-    <a v-if="currentLang === 'en'" href="/CV ENG.pdf" target="_blank">Download CV</a>
-    <a v-else href="/CV FR.pdf" target="_blank">Télécharger CV</a>
+    <a v-if="currentLang === 'en'" href="/CV ENG.pdf" download="CV.pdf" target="_blank"
+      >Download CV</a
+    >
+    <a v-else href="/CV FR.pdf" download="CV.pdf" target="_blank">Télécharger CV</a>
   </button>
 </template>
